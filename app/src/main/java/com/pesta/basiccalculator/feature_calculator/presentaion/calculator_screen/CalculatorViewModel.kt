be568544572c525ae73a80a8e25363c437d9db2f
pesta <kceknm@gamil.com>
2024-10-14 20:03:34 +0300

    implemented the CalculatorViewModel functions.

--- app/src/main/java/com/pesta/basiccalculator/feature_calculator/presentaion/calculator_screen/CalculatorViewModel.kt
+++ app/src/main/java/com/pesta/basiccalculator/feature_calculator/presentaion/calculator_screen/CalculatorViewModel.kt
@@ -1,7 +1,38 @@
 package com.pesta.basiccalculator.feature_calculator.presentaion.calculator_screen
 
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pesta.basiccalculator.feature_calculator.domain.use_cases.AppendToExpressionUseCase
+import com.pesta.basiccalculator.feature_calculator.domain.use_cases.GetExpressionUseCase
+import com.pesta.basiccalculator.feature_calculator.domain.use_cases.RestExpressionUseCase
+import com.pesta.basiccalculator.feature_calculator.domain.use_cases.UndoLastEntryUseCase
+import com.pesta.basiccalculator.feature_calculator.domain.use_cases.UpdateExpressionUseCase
+import kotlinx.coroutines.launch
 
-class CalculatorViewModel: ViewModel() {
+class CalculatorViewModel(
+    private val appendToExpression: AppendToExpressionUseCase,
+    getExpression: GetExpressionUseCase,
+    private val undoLastEntry: UndoLastEntryUseCase,
+    private val reset: RestExpressionUseCase
+): ViewModel() {
+    val expressionFlow = getExpression()
 
+    fun append(value: String){
+        viewModelScope.launch {
+            appendToExpression(value.first())
+        }
+    }
+
+    fun deleteSingleEntry(){
+        viewModelScope.launch {
+            undoLastEntry()
+        }
+    }
+
+    fun clearExpression(){
+        viewModelScope.launch {
+            reset()
+        }
+    }
 }
